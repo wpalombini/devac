449030d3724881f748999f78c5ec4d96c78bd65b
Walter Palombini <walterpalombini@gmail.com>
2021-05-24 21:18:19 +1000

    Initial Mint method

--- src/contracts/DeVacContract.sol
+++ src/contracts/DeVacContract.sol
@@ -1,6 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-contract DeVacContract {
-    
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract DeVacContract is ERC721 {
+    uint256 private lastTokenIdGenerated = 0;
+
+    constructor() ERC721("DeVac", "DVac") { }
+
+    function mint(address _to) public returns (uint256) {
+        uint256 newTokenId = lastTokenIdGenerated + 1;
+        
+        _mint(_to, newTokenId);
+
+        return newTokenId;
+    }
 }
--- test/DeVacContractTests.js
+++ test/DeVacContractTests.js
@@ -3,14 +3,37 @@ const truffleAssert = require('truffle-assertions');
 const DeVacContract = artifacts.require('DeVacContract.sol');
 
 contract('DeVacContract', (accounts) => {
+  let deVacContract;
+
+  const getRandomNumber = () => {
+    return Math.floor(Math.random() * 10);
+  };
+
+  before(async () => {
+    deVacContract = await DeVacContract.deployed();
+  });
+
   describe('deployment', async () => {
     it('deploys successfully', async () => {
-      const address = await DeVacContract.address;
+      const address = await deVacContract.address;
 
       assert.notEqual(address, 0x0);
       assert.notEqual(address, '');
       assert.notEqual(address, null);
       assert.notEqual(address, undefined);
+    });
+  });
+
+  describe('mint', async () => {
+    it('mints new a token successfully', async () => {
+      const randomAccount = accounts[getRandomNumber()];
+      const result = await deVacContract.mint(randomAccount);
+
+      const event = result.logs[0].args;
+
+      assert.equal(event.tokenId.toNumber(), 1);
+      assert.equal(event.from, '0x0000000000000000000000000000000000000000');
+      assert.equal(event.to, randomAccount);
     });
   });
 });
